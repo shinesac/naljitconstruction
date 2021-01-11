cb2e4f98662366f5166162b2ffa74bcb1600c320
Shinesa Chowdhury <shinesac@yahoo.com>
2021-01-11 13:09:43 -0500

    script added and media queries

---
+++ script.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+	document.querySelector(".menu").classList.toggle("change");
+    document.getElementById("list").classList.toggle("change");
+    document.getElementById("arrow").classList.toggle("switch")
+}
